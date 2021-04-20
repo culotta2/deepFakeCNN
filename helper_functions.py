22b39895c401dc590d24f7360046cec21fefb341
Dominic Culotta <culotta2@illinois.edu>
2021-04-20 14:59:59 -0500

    Added functionality for saving image data to numpy arrays

--- helper_functions.py
+++ helper_functions.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 
 
-def get_images(n: int, dtype: str = 'train', seed: int = None):
+def get_images(n: int, dsplit: str = 'train', seed: int = None):
     '''
     Returns n randomly selected testing, training, or validation data.
 
@@ -14,23 +14,23 @@ def get_images(n: int, dtype: str = 'train', seed: int = None):
     '''
 
     # Make sure train param is valid
-    if dtype not in ['train', 'valid', 'test']:
+    if dsplit not in ['train', 'valid', 'test']:
         raise Exception("dtype argument must be train, valid, or test.")
 
     # Load labeled dataframes
     PATHDIR = Path('data')
-    df = pd.read_csv(PATHDIR / f'{dtype}.csv', header=0).drop(
+    df = pd.read_csv(PATHDIR / f'{dsplit}.csv', header=0).drop(
         ['original_path', 'Unnamed: 0', 'label_str'], axis=1)
 
     df_real = df[df['label'] == 0]
     df_fake = df[df['label'] == 1]
 
     # Get the number of files in the directory of interest
-    n_files = {"train": 50000, "valid": 10000, "test": 10000}[dtype]
+    n_files = {"train": 50000, "valid": 10000, "test": 10000}[dsplit]
 
     # Make sure you don't want more pictures than we have
     if n > n_files:
-        raise Exception(f'There are not {n} files in the {dtype} folder')
+        raise Exception(f'There are not {n} files in the {dsplit} folder')
 
     # Set a seed if present
     if seed is not None:
@@ -76,10 +76,56 @@ def prep_for_train(sample_df):
     return X, y[np.newaxis].reshape(-1, 1)
 
 
+def imgs_to_numpy(dsplit):
+    """
+    Saves the images to prevent them having to be loaded
+    as numpy arrays later
+    """
+    # Check that the split exists
+    if dsplit not in ('train', 'valid', 'test'):
+        raise Exception('dsplit must be `train`, `test`, or `valid`')
+
+    # Load labeled dataframes
+    PATHDIR = Path('data')
+    DATADIR = Path('data/') / 'real_vs_fake' / 'real-vs-fake'
+    SAVEPATH = PATHDIR / 'data_array'
+
+    df = pd.read_csv(PATHDIR / f'{dsplit}.csv', header=0).drop(
+        ['original_path', 'Unnamed: 0', 'label_str'], axis=1).head()
+
+    # Create containers for the image data
+    n = df.shape[0]
+    X = np.empty(shape=(n, 256, 256, 3))
+    y = df['label'].to_numpy()[np.newaxis].reshape(-1, 1)
+
+    # Load in the images
+    for img_idx, img_path in enumerate(df['path']):
+        img = plt.imread(DATADIR / img_path)
+        X[img_idx, :, :, :] = img / 255.0
+
+    # Save the images as numpy arrays
+    with open(SAVEPATH / f'X_{dsplit}.npy', 'wb') as file:
+        np.save(file, X)
+
+    with open(SAVEPATH / f'y_{dsplit}.npy', 'wb') as file:
+        np.save(file, y)
+
+
 def main():
     """
     Function for testing
     """
+    # imgs_to_numpy('train')
+
+    # with open('data/data_array/X_train.npy', 'rb') as f:
+    #     X = np.load(f)
+
+    # with open('data/data_array/y_train.npy', 'rb') as f:
+    #     y = np.load(f)
+
+    # plt.imshow(X[1, :, :, :])
+    # print(X[1, :, :, :].shape)
+    # plt.show()
 
 
 if __name__ == '__main__':
